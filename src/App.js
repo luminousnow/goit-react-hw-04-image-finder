44ac719ea4f643a93b0e7beb752f790095f36d56
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2020-12-27 00:53:50 +0200

    refactoring code by using hooks

--- src/App.js
+++ src/App.js
@@ -1,63 +1,42 @@
 import React from 'react';
-import { Component } from 'react';
+import { useState } from 'react';
 import { ToastContainer } from 'react-toastify';
 import s from './App.module.css';
 import Searchbar from './Components/Searchbar/Searchbar';
 import Modal from './Components/Modal/Modal';
 import ImageGallery from './Components/ImageGallery/ImageGallery';
 
-class App extends Component {
-  state = {
-    imgName: '',
-    showModal: false,
-    lrgImg: '',
-    altImg: '',
+const App = () => {
+  // === State === //
+  const [imgName, setImgName] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [lrgImg, setLrgImg] = useState('');
+  const [altImg, setAltImg] = useState('');
+
+  // reverce showModal state
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
-  // отримує і записує в imgName назву зображення
-  getImgName = submitImgName => {
-    this.setState({ imgName: submitImgName });
+  // get large img, alt & change modal state to "true"
+  const getModalData = (gettinglargeImg, gettingAltImg) => {
+    setLrgImg(gettinglargeImg);
+    setAltImg(gettingAltImg);
+    setShowModal(true);
   };
 
-  // змінює стан showModal
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({ showModal: !showModal }));
-  };
+  return (
+    <div className={s.app}>
+      <ToastContainer position="bottom-center" autoClose={2500} />
+      <Searchbar onSubmit={setImgName} />
 
-  getModalData = (gettinglargeImg, gettingAltImg) => {
-    this.setState({
-      lrgImg: gettinglargeImg,
-      altImg: gettingAltImg,
-      showModal: true,
-    });
-  };
+      <ImageGallery imgName={imgName} getModalData={getModalData} />
 
-  render() {
-    const { getImgName, toggleModal, getModalData } = this;
-    const { imgName, showModal, lrgImg, altImg } = this.state;
-
-    return (
-      <div className={s.app}>
-        <ToastContainer position="bottom-center" autoClose={2500} />
-        <Searchbar onSubmit={getImgName} />
-
-        {/* рендерить ImageGallery по умові */}
-        <ImageGallery imgName={imgName} getModalData={getModalData} />
-
-        {/* рендерить Modal по умові */}
-        {showModal && (
-          <Modal toggleModal={toggleModal} largeImg={lrgImg} altImg={altImg} />
-        )}
-      </div>
-    );
-  }
-}
-
-// починати завантаження нової коллекції з 1
-// переміщувати екран при кліку на кнопку Лоад мо
-// Приховати розмітку в залежності від довжини колекції
-// Показувати кнопку в залежності від довжини масиву
-// error https://youtu.be/xoG3l2PgiYY?t=3024
-// Модалка https://www.youtube.com/watch?v=w6MW1szKuT4&feature=youtu.be&t=1383
+      {showModal && (
+        <Modal toggleModal={toggleModal} largeImg={lrgImg} altImg={altImg} />
+      )}
+    </div>
+  );
+};
 
 export default App;
