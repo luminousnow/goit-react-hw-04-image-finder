0cff2a8a5075bbf81777091e8366ece1577bec71
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2020-12-30 23:14:10 +0200

    змінює назву колбеку

--- src/Components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/Components/ImageGallery/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import s from './ImageGalleryItem.module.css';
 
-function ImageGalleryItem({ smallImg, largeImg, alt, onClick }) {
+function ImageGalleryItem({ smallImg, largeImg, alt, onImageClick }) {
   return (
     <li className={s.imageGalleryItem}>
       <img
         src={smallImg}
         data-original={largeImg}
-        onClick={onClick}
+        onClick={onImageClick}
         alt={alt}
         className={s.imageGalleryItem__image}
       />
